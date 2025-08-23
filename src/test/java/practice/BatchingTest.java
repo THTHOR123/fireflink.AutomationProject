952fecde7e3eec424b594d04114d5b42ae3e5a1b
Tushar Jadhav <Tushar Jadhav@FFE-Tushar-Jadhav>
2025-08-23 11:58:14 +0530

    class created

---
+++ src/test/java/practice/BatchingTest.java
@@ -0,0 +1,11 @@
+package practice;
+
+import org.testng.annotations.Test;
+
+public class BatchingTest {
+
+	@Test
+	public void run() {
+		System.out.println("running the Batching build");
+	}
+}
---
+++ src/test/java/practice/RegressionTest.java
@@ -0,0 +1,11 @@
+package practice;
+
+import org.testng.annotations.Test;
+
+public class RegressionTest {
+
+	@Test
+	public void sample() {
+		System.out.println("Sample is running");
+	}
+}
